d58292c6d8b4e186fff2d919da633a9487036809
Anja Seeba <ahoi@achtsam.media>
2021-03-03 12:09:47 +0100

    Set up the server

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express')
-
 const app = express()
-
-app.use((req, res) => {
-    console.log(req.method, req.url)
-    res.end('Hello world')
+app.use('/api/users', (req, res) => {
+  res.end('[{"name": "Melissa", "role": "student"}]')
 })
-
+app.use('/api/cards', (req, res) => {
+  res.end('[{"title": "First card"}]')
+})
 app.listen(3000, () => {
-    console.log('Server started at http://localhost:3000')
+  console.log('Server started at http://localhost:3000')
 })
