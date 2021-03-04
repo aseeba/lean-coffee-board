cd651ad11b230ed18a5454d3d81789b61065b6a3
Anja Seeba <ahoi@achtsam.media>
2021-03-04 11:13:51 +0100

    Add Card

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Card = require('./models/Card')
 const User = require('./models/User')
 
 mongoose
@@ -10,32 +11,46 @@ mongoose
   .then(() => console.log('Connected to mongodb'))
   .catch(error => console.error('Could not connect to mongodb', error))
 
-  const app = express()
+const app = express()
 
 app.use(express.json()) // add middleware for json data
 
 app.get('/api/users', async (req, res) => {
-    // with then:
-    // User.find().then(users => res.json(users))
-  
-    // with async/await
-    res.json(await User.find())
-  })
+  res.json(await User.find())
+})
 
-  app.get('/api/users/:id', async (req, res) => {
-    app.delete('/api/users/:id', async (req, res) => {
-        res.json(await User.deleteOne({ id }))
-    })
+app.get('/api/users/:id', async (req, res) => {
+  const { id } = req.params
+  res.json(await User.findOne({ id }))
+})
 
+app.delete('/api/users/:id', async (req, res) => {
+  const { id } = req.params
+  res.json(await User.deleteOne({ id }))
+})
 
+app.post('/api/users', async (req, res) => {
+  res.json(await User.create(req.body))
+})
 
-    app.post('/api/users', async (req, res) => {
-        res.json(await User.create(req.body))
-      })
-      
-      app.get('/api/cards', (req, res) => {
-        res.json([{ title: 'First card' }])
-      })
-      app.listen(3000, () => {
-        console.log('Server started at http://localhost:3000')
-      })
+app.get('/api/cards', async (req, res) => {
+  res.json(await Card.find().populate('author'))
+})
+
+app.get('/api/cards/:id', async (req, res) => {
+  const { id } = req.params
+  res.json(await Card.findOne({ id }).populate('author'))
+})
+
+app.delete('/api/cards/:id', async (req, res) => {
+  const { id } = req.params
+  res.json(await Card.deleteOne({ id }))
+})
+
+app.post('/api/cards', async (req, res) => {
+  res.json(await Card.create(req.body))
+})
+
+app.listen(3000, () => {
+  console.log('Server started at http://localhost:3000')
+})
