e766f6e80f9cb419398b55ea93576e395d6e3713
Anja Seeba <ahoi@achtsam.media>
2021-03-04 10:39:39 +0100

    Interim State

--- models/User.js
+++ models/User.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  role: String,
-})
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    role: String,
+  },
+  { versionKey: false }
+)
 
 module.exports = mongoose.model('User', userSchema)
